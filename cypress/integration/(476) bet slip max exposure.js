e70bb92125d3f364b08327d87af8ddd97d38fd83
augustesteponenaite <auguste.steponenaite@gmail.com>
2022-01-21 16:47:01 +0200

    rule max exposure

--- cypress/integration/(476) bet slip max exposure.js
+++ cypress/integration/(476) bet slip max exposure.js
@@ -5,6 +5,17 @@ describe('BB+widgets' , function ()
         cy.wait(2000);
         cy.intercept('GET','/api/player/playersByGame*').as(`arq`)
         cy.get('.ligues-slider__ligue-name').eq(2).click()
-        cy.get('.icon.icon-header-dropdown').first().click()
+        cy.wait(2000)
+        cy.get('.main-markets__item').eq(1).click()
+        cy.get('.tiered-block__item').first().click()
+        cy.wait(2000)
+        cy.intercept('POST','/api/cm-exposure').as(`exp`)
+        cy.get('.shots-slider__item.sliderItem.hoverStyle').eq(2).click()
+        cy.wait('@exp',{responseTimeout:10000}).its('response.body').should('not.be.null')
+        cy.get('@exp').its('response.body').as('exp_res')
+            .should('have.property', 'data')
+
+
+
     })
 })
