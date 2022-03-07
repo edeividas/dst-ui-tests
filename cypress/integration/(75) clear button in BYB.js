6bf5714880fd87c84cbabae07cb81f0293b81a96
Peter McKenna <peter@digitalsportstech.com>
2022-03-07 15:51:11 +0000

    cypress config baseUrl, updates for tests 205 and 75

--- cypress/integration/(75) clear button in BYB.js
+++ cypress/integration/(75) clear button in BYB.js
@@ -1,16 +1,13 @@
-describe('BB+widgets' , function ()
-{
+describe('BB+widgets', function () {
     it('clear button in BYB', function () {
-        cy.visit('https://widgets-bm.dev.digitalsportstech.com/betbuilder?sb=test')
+        cy.visit('/betbuilder?sb=test&demo=test');
         cy.wait(5000);
-        cy.intercept('GET','/api/player/playersByGame*').as(`arq`)
-        cy.get('.banner-menu__build-button').click()
-        cy.wait('@arq',{responseTimeout:30000}).its('response.body').should('not.be.null');
-        cy.get('.icon.icon-add').eq(0).click()
+        cy.intercept('GET', '**/api/player/playersByGame*').as(`arq`);
+        cy.get('.banner-menu__build-button').click();
+        cy.wait('@arq').its('response.body').should('not.be.null');
+        cy.get('.icon.icon-add').eq(0).click();
         cy.wait(2000);
-        cy.get('.build-form__clear-all').click()
+        cy.get('.build-form__clear-all').click();
 
-
-
-    })
-})
+    });
+});
