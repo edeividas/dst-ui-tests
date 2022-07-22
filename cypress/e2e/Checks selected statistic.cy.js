0460fb79068b2e97d1128f4c5d59ac7e247b5dee
edeividas <103195877+edeividas@users.noreply.github.com>
2022-07-22 16:59:51 +0300

    v1

---
+++ cypress/e2e/Checks selected statistic.cy.js
@@ -0,0 +1,32 @@
+describe('Checks selected statistic ar show all', () => {
+    it('checks selected market',() => {
+        cy.visit('/betbuilder?sb=test&demo=test&user=demo')
+        cy.get('.ligues-slider__item--selected')
+        const kiek =  cy.get('div.ligues-slider__item--selected > .ligues-slider__count ')
+            .invoke('text')
+            .then(parseInt)
+            .then((kiek) =>{
+               cy.log(kiek)
+
+               if(kiek === 1) {
+                   cy.log('kiek')
+                   cy.get('.main-markets__item').should('be.visible')
+                       .contains('All Markets')
+                       .parent()
+                       .should('have.class', 'main-markets__item--active')
+               }
+                   if (kiek > 1) {
+                       cy.get('.games-selection__select').click()
+                       cy.get('.games-selection__dropdown').first().click()
+
+                   }
+                   if( kiek  === 0) {
+
+                       cy.log("Something wrong - No games on League")
+
+                   }
+           })
+
+   })
+
+})
