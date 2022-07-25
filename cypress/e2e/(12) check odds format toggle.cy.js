121a3fc26f53ada4f556a2c80aa290dda7baabb0
edeividas <103195877+edeividas@users.noreply.github.com>
2022-07-25 09:31:47 +0300

    modified 07.25

--- cypress/e2e/(12) check odds format toggle.cy.js
+++ cypress/e2e/(12) check odds format toggle.cy.js
@@ -1,14 +1,20 @@
 describe('Checks dds format toggle Decimal/American',() => {
 
    it('Checks odds american format ',  () => {
-       cy.visit('/betbuilder?sb=test&demo=test&user=deividas1&oddsType=american')
+       cy.visit('/betbuilder?sb=test&demo=test&user=demo&oddsType=american')
 
-       cy.get('.games-dropdown__item').click({force: true})
+       cy.get('.games-selection__select').click()
+       cy.get('.games-selection__dropdown').first().click()
        cy.wait(1000)
-       cy.get('.games-dropdown__item').first().next().click()
-       cy.get('div > .over-under-block__selector ').first()
+       cy.get('.banner-menu__players').first().click({force:true}).click()
+       cy.get('[type="checkbox"]').first().check()
+       // cy.wait(1000)
+       cy.get('.banner-menu__players').first().click()
+       cy.get('div >.over-under-block__selector ')
+       cy.get('.over-under-block__selector').contains('Over').first()
        cy.get('.over-under-block__selector-value').first()
-               .invoke('text')
+           .invoke('text')
+                //Regex number with symbol (-/+)
                .should('match', / \+|-/)
                .then(parseInt)
                .should('be.a', 'number')
@@ -18,15 +24,21 @@ describe('Checks dds format toggle Decimal/American',() => {
     it('Chceks decimal odds format ',() => {
 
         cy.visit('/betbuilder?sb=test&demo=test&user=deividas1&oddsType=decimal')
-        cy.get('.games-dropdown__item').click({force: true})
+        cy.get('.games-selection__select').click()
+        cy.get('.games-selection__dropdown').first().click()
         cy.wait(1000)
-        cy.get('.games-dropdown__item').first().next().click()
-        cy.get('div > .over-under-block__selector ').first()
+        cy.get('.banner-menu__players').first().click({force:true}).click()
+        cy.get('[type="checkbox"]').first().check()
+        // cy.wait(1000)
+        cy.get('.banner-menu__players').first().click()
+        cy.get('div >.over-under-block__selector ')
+        cy.get('.over-under-block__selector').contains('Over').first()
         cy.get('.over-under-block__selector-value').first()
             .invoke('text')
             .then(parseInt)
             .should('be.a', 'number')
             .should('be.gte', 1)
+            //check regex number is 1-100 in interval
             .should('match', /^([1-9][0-9]?|100)$/)
 
     })
