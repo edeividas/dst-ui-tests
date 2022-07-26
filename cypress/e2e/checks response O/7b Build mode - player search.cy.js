6bf3ca2d2bcca485ba769ad9be0b4f6c767d3bbc
edeividas <103195877+edeividas@users.noreply.github.com>
2022-07-26 13:33:54 +0300

    player search field check

---
+++ cypress/e2e/checks response O/7b Build mode - player search.cy.js
@@ -0,0 +1,33 @@
+describe('BB+widgets' , function ()
+{
+    it('player search field', function () {
+        cy.visit('https://widgets-bm.dev.digitalsportstech.com/betbuilder?sb=test&demo=true')
+        cy.wait(1000);
+        cy.get('.banner-menu__build-button').click()
+        cy.get('.games-selection').click()
+        cy.get('app-game-filter-title > :nth-child(1) > :nth-child(2)').click()
+        cy.wait(1000)
+        cy.intercept('GET','**/api/player/playersByGame*').as(`res`)
+        cy.wait('@res').its('response.body').should('be.an','array')
+            .then((response) => {
+
+                let data = response
+                let resultdata = [];
+                let name = "";
+
+                for (let i= 0 ; i<data.length ; i++){
+
+                    resultdata.push({'name': data[i].name})
+
+                    if(resultdata[0].name !== "" || resultdata[0].name !== null){
+                      name = resultdata[0].name
+                    }
+                }
+                cy.log(resultdata[0].name)
+                cy.get('.players-dropdown__search').type(name)
+                cy.get('.player-searched__icon > .icon').click()
+                cy.get('.player-statistics__header > p').should('contain', name)
+            })
+
+    })
+})
