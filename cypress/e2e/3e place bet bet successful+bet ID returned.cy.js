2ef914f5b84ba20a53f4c4cfb443264fb22f60f6
edeividas <103195877+edeividas@users.noreply.github.com>
2022-08-01 16:19:32 +0300

    .

--- cypress/e2e/3e place bet bet successful+bet ID returned.cy.js
+++ cypress/e2e/3e place bet bet successful+bet ID returned.cy.js
@@ -42,6 +42,7 @@ describe('Place bet: checks bet successful + bet ID response', () => {
                     cy.get('@betid').should('not.be.null')
                         .should('match',/^[0-9]*[1-9][0-9]*$/) //regex for positive integers
 
+
                 }
 
             })
