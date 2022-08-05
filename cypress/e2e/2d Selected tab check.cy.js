403d93e82fe72596d3601475f6274683df60bdc6
edeividas <103195877+edeividas@users.noreply.github.com>
2022-08-05 16:51:05 +0300

    selected tab check

--- cypress/e2e/2d Selected tab check.cy.js
+++ cypress/e2e/2d Selected tab check.cy.js
@@ -9,20 +9,30 @@ describe('Checks selected statistic ar show all', () => {
                cy.log(kiek)
 
                if(kiek === 1) {
-                   cy.log('kiek')
+
                    cy.get('.main-markets__item').should('be.visible')
                        .contains('All Markets')
                        .parent()
                        .should('have.class', 'main-markets__item--active')
+                   cy.log("Selected statistic is:\'All Markets\'")
                }
                    if (kiek > 1) {
+
+                       cy.get('.main-markets__item.main-markets__item--active').should('have.class', 'main-markets__item--active')
+                           .invoke('text')
+                           .then((text) => {
+                               cy.log(JSON.stringify(text))
+                               cy.log('Selected market is:' + ' ' + text)
+
+                           })
+
                        cy.get('.games-selection__select').click()
                        cy.get('.games-selection__dropdown').first().click()
                        cy.get('app-game-filter-title > div > div').its('length')
-                           .should('be.greaterThan',0)
-
+                          .should('be.gte',1)
 
                    }
+
                    if( kiek  === 0 ) {
 
                        cy.log("Something wrong - No games on League")
